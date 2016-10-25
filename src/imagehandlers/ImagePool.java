93a9412a13c74b0a2c9c38c5382c19fe1a7b16c4
Anthony <94aharris@gmail.com>
2016-10-25 11:27:42 -0400

    Modify header information

--- src/imagehandlers/ImagePool.java
+++ src/imagehandlers/ImagePool.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package imagehandlers;
 
 import java.awt.Graphics2D;
@@ -14,9 +9,10 @@ import java.util.ArrayList;
 import javax.imageio.ImageIO;
 
 /**
- *
- * @author aharris
+ * @author Anthony Harris
+ * @repository https://github.com/94aharris/MiniPicEditor
  */
+
 public class ImagePool {
   ArrayList <ImageObject> images;
   
