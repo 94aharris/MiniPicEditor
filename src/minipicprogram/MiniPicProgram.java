235d7108dedfc9797d65bcbb10aae3cffb401d68
Anthony <94aharris@gmail.com>
2016-10-25 11:25:01 -0400

    Modified header information

--- src/minipicprogram/MiniPicProgram.java
+++ src/minipicprogram/MiniPicProgram.java
@@ -1,15 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package minipicprogram;
 
 import ui.MiniPicGUI;
 
 /**
- *
- * @author aharris
+ * @author Anthony Harris
+ * @repository https://github.com/94aharris/MiniPicEdito
  */
 public class MiniPicProgram {
 
